ce6bb12a3cab16334f605ca13c35923f2104c48b
yony-bentancor <yonybentancor@gmail.com>
2025-04-19 20:46:06 -0300

    UPD

--- models/BodegaCentral.js
+++ models/BodegaCentral.js
@@ -1,17 +1,19 @@
-// models/BodegaCentral.js
 const mongoose = require("mongoose");
 
-const bodegaCentralSchema = new mongoose.Schema({
-  componentes: [
-    {
-      componente: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Componente",
-        required: true,
+const bodegaCentralSchema = new mongoose.Schema(
+  {
+    componentes: [
+      {
+        componente: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Componente",
+          required: true,
+        },
+        cantidad: { type: Number, default: 0 },
       },
-      cantidad: { type: Number, default: 0 },
-    },
-  ],
-});
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("BodegaCentral", bodegaCentralSchema);
--- models/Movimiento.js
+++ models/Movimiento.js
@@ -1,25 +1,34 @@
 const mongoose = require("mongoose");
 
-const movimientoSchema = new mongoose.Schema({
-  fecha: {
-    type: Date,
-    default: Date.now,
-  },
-  origen: {
-    tipo: { type: String, enum: ["usuario", "bodega"], required: true },
-    id: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // null si es bodega
-  },
-  destino: {
-    tipo: { type: String, enum: ["usuario", "bodega"], required: true },
-    id: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // null si es bodega
-  },
-  componentes: [
-    {
-      componente: { type: mongoose.Schema.Types.ObjectId, ref: "Componente" },
-      cantidad: Number,
+const movimientoSchema = new mongoose.Schema(
+  {
+    fecha: {
+      type: Date,
+      default: Date.now,
     },
-  ],
-  comentario: String, // opcional
-});
+    origen: {
+      tipo: {
+        type: String,
+        enum: ["usuario", "bodega", "almacen"],
+        required: true,
+      }, // agregué "almacen"
+      id: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // null si es bodega o almacén
+    },
+    destino: {
+      tipo: { type: String, enum: ["usuario", "bodega"], required: true },
+      id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    },
+    componentes: [
+      {
+        componente: { type: mongoose.Schema.Types.ObjectId, ref: "Componente" },
+        cantidad: Number,
+      },
+    ],
+    comentario: String,
+  },
+  {
+    timestamps: true, // 👈 activa createdAt y updatedAt automáticamente
+  }
+);
 
 module.exports = mongoose.model("Movimiento", movimientoSchema);
